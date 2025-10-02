f049ce6385c463ea9075e3145b2897c5f6485cd9
nandadulalkumbhakar <nandadulalkumbhakar@gmail.com>
2025-10-02 22:30:13 +0530

    date and number

---
+++ 01_basic/NumberAndMath.js
@@ -0,0 +1,10 @@
+let a = 1000;
+console.log(a);//100
+let b = new Number(2000);
+console.log(b);//[Number: 2000]
+console.log(b.toString());//2000
+console.log(a.toString().length);//4
+console.log(b.toFixed(2));//2000.00
+
+
+
