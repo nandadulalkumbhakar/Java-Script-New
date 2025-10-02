7a1605e532464e665947c3cc0061c27ee63787fc
nandadulalkumbhakar <nandadulalkumbhakar@gmail.com>
2025-10-02 10:56:25 +0530

    String

---
+++ 01_basic/String.js
@@ -0,0 +1,16 @@
+let name = "Nanda Dulal Kumbhakar";
+console.log(name);
+let arr=name.split(" ")
+console.log(arr);
+let sub = name.substring(0, 3);
+console.log(sub);
+console.log(name.toUpperCase());
+console.log(name.toLowerCase())
+for (let i = 0; i < name.length; i++)
+    console.log(name.charAt(i));
+
+console.log(name.indexOf('n'));
+
+    
+
+
