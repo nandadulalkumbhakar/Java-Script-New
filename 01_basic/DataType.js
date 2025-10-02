7ff210a8e00a25485f4f2e0a6ec58b6323356184
nandadulalkumbhakar <nandadulalkumbhakar@gmail.com>
2025-10-02 09:02:59 +0530

    add none primitive datatype

--- 01_basic/DataType.js
+++ 01_basic/DataType.js
@@ -2,25 +2,19 @@ let a = 15;
 let  b = 'Nanda';
 let c = "Koushik";
 let d = true;
-console.log(typeof (a));//number
+/*console.log(typeof (a));//number
 console.log(typeof (b));//string
 console.log(typeof (c));//string
 console.log(typeof (d));//boolean
 console.log(typeof (null));//object
 console.log(typeof (undefined));//undefine
-console.log(typeof(Symbol));//function
-
-
-
-
-
-
-
-
-
+console.log(typeof(Symbol));//function*/
 
 /*
-Data Type in JS: -
+Data Type in JS: - i) primitive ii) non primitive or reference
+
+    i)primitive
+
     1) Number=> Range(-2^52-1 to 2^53-1)
     2) BigInt=>12345678901234567890123456789n
     3) string
@@ -28,4 +22,34 @@ Data Type in JS: -
     5) null=>standalone value 
     6) undefined 
     7) symbol => unique
+
+    ii)non primitive 
+
+    1)Array
+    2)object
+    3)function
 */
+
+//non primitive
+
+let arr = [1, 2, 3, 4, 5];// Array in js
+console.log(arr);
+for (let i = 0; i < arr.length;i++)
+{
+    console.log(arr[i]);
+    
+}
+
+//object
+let obj = {
+    name: "Nanda",
+    roll: 122,
+    age: 23
+};
+
+function myfunction() 
+{
+    console.log("Hello World"); 
+    
+}
+myfunction();
