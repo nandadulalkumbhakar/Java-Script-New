ada877369ce2c9db5bdf818b77e88fc59791ab8e
nandadulalkumbhakar <nandadulalkumbhakar@gmail.com>
2025-10-03 09:00:48 +0530

    change folder name

---
+++ JS basic/Object.js
@@ -0,0 +1,24 @@
+// object is key value pair
+
+let student = {
+    name: "Nanda",
+    email:"nanda@gmail.com",
+    roll: 123,
+    dept: "CSE",
+    reg: 2210800,
+}
+//console.log(student);
+//student.name = "Dulal"//modify the value
+//Object.freeze(student);//no one can't modify the value of object
+//student.email = "dulal@gmail.com"
+//console.log(student);
+console.log(`Hello ${student.name} How are You?`);
+
+/*type of object: -
+    1)Singleton object, eg: - let stu = new Object()
+    2)multiton Object,  eg:- let stu={}
+*/
+
+
+
+
